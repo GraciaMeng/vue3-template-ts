4f5b2619f3076d18c3c22ce1d8ab110ef58e88f4
MengJiaXi <1040616435@qq.com>
2021-11-15 22:48:50 +0800

    feat: add vuex types and overall situation use store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,12 @@
-import { createStore } from "vuex";
+import { createStore, Store } from "vuex";
+import { InjectionKey } from "vue";
+
+export const key: InjectionKey<Store<State>> = Symbol();
+
+// vuex类型声明
+export type State = {
+  count: number;
+};
 
 export default createStore({
   state: {},
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,4 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "Home",
-  components: {},
-});
-</script>
+<script setup lang="ts"></script>
